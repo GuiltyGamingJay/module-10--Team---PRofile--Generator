1fd3e89bef49a83d86e9237467dab06d7e9f3fed
GuiltyGamingJay <jerontai07@gmail.com>
2023-01-18 12:02:46 -0500

    Employee,Enginneer,and intern questions

--- index.js
+++ index.js
@@ -43,4 +43,130 @@ function init() {
       })
       .catch((err) => console.log(err));
   }
+//Question to add more employee
+function addMoreEmployee() {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          message: "Would you like to add another employee?",
+          choices: ["yes", "no"],
+          name: "addEmployee",
+        },
+      ])
+      .then((res) => {
+        if (res.addEmployee === "yes") {
+          typeofEmployee();
+        } else {
+          const html = getHtml();
+          fs.writeFile("./dist/my-team.html", html, (err) =>
+            err
+              ? console.log(err)
+              : console.log("Your team profile has been created")
+          );
+        }
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  }
+  
+  //Question to add a specific type of employee
+  function typeofEmployee() {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          message: "What kind of employee would you like to add?",
+          choices: ["Manager", "Engineer", "Intern"],
+          name: "jobTitle",
+        },
+      ])
+      .then((res) => {
+        if (res.jobTitle === "Manager") {
+          init();
+        } else if (res.jobTitle === "Engineer") {
+          addEngineer();
+        } else if (res.jobTitle === "Intern") {
+          addIntern();
+        }
+      });
+  }
+  
+  //Questions for Engineer Role
+  function addEngineer() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What is the engineer's name?",
+          name: "engineerName",
+        },
+        {
+          type: "input",
+          message: "What is the engineer's employee Id?",
+          name: "engineerId",
+        },
+        {
+          type: "input",
+          message: "What is the engineer's email address?",
+          name: "engineerEmail",
+        },
+        {
+          type: "input",
+          message: "What is the engineer's Github username?",
+          name: "engineerGithub",
+        },
+      ])
+      .then((res) => {
+        let engineer = new Engineer(
+          res.engineerName,
+          res.engineerId,
+          res.engineerEmail,
+          res.engineerGithub
+        );
+        console.log(engineer);
+        employeeList.push(engineer);
+        addMoreEmployee();
+      })
+      .catch((err) => console.log(err));
+  }
+  
+  //Questions for Intern Role
+  function addIntern() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What is the intern's name?",
+          name: "internName",
+        },
+        {
+          type: "input",
+          message: "What is the intern's employee Id?",
+          name: "internId",
+        },
+        {
+          type: "input",
+          message: "What is the intern's email address?",
+          name: "internEmail",
+        },
+        {
+          type: "input",
+          message: "What is the intern's school?",
+          name: "internSchool",
+        },
+      ])
+      .then((res) => {
+        let intern = new Intern(
+          res.internName,
+          res.internId,
+          res.internEmail,
+          res.internSchool
+        );
+        console.log(intern);
+        employeeList.push(intern);
+        addMoreEmployee();
+      })
+      .catch((err) => console.log(err));
+  }
   
